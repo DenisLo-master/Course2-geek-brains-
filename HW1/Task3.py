8be5d2604564713f4fb98ac1f62cda076c323d6b
DenisLo-master <66447463+DenisLo-master@users.noreply.github.com>
2021-10-04 23:26:33 +0000

    Обработка исключений перенесена в функцию

--- HW1/Task3.py
+++ HW1/Task3.py
@@ -1,48 +1,50 @@
 #3. По введенным пользователем координатам двух точек вывести уравнение прямой вида y=kx+b, проходящей через эти точки.
 
+
 def equation(x1, y1, x2, y2):
-    k = (y1 - y2) / (x1 - x2)
-    b = y2 - k * x2
-    return k, b
+    try:
+        if x1 == x2:
+            raise ZeroDivisionError
+        k = (y1 - y2) / (x1 - x2)
+        b = y2 - k * x2
+        return k, b
+    except ZeroDivisionError:
+        print('\nОШИБКА деления на 0: значения x1 и x2 совпадают')
+        exit()
+
 
 def test():
-    assert equation(4,2,3,2) == (0, 2), 'incorrect'
-    assert (0.8,-2.2) <=  equation(9,5,4,1) <= (0.81, -2.21), 'incorrect'
-    assert (0.625,1.375) <=  equation(1,2,9,7) <= (0.626, 1.376), 'incorrect'
-    assert equation(1,10,2,12) == (2,8), 'incorrect'
-    print('Test: OK')
+    assert equation(4, 2, 3, 2) == (0, 2), 'incorrect'
+    assert (0.8, -2.2) <= equation(9, 5, 4, 1) <= (0.81, -2.21), 'incorrect'
+    assert (0.625, 1.375) <= equation(1, 2, 9,
+                                      7) <= (0.626, 1.376), 'incorrect'
+    assert equation(1, 10, 2, 12) == (2, 8), 'incorrect'
+
+    #assert equation(1, 10, 1, 12) == False, 'incorrect'
+
+    print('Test: OK\n')
 
 
 test()
 
 try:
-    x1,y1=input('Введите координаты ПЕРВОЙ точки (x;y): ').split(';')
-    x1,y1=map(float,(x1,y1))
+    x1, y1 = map(float,
+                 input('Введите координаты ПЕРВОЙ точки (x;y): ').split(';'))
 except ValueError:
-    print('ОШИБКА: Введите х и y координаты, в формате ’x;y’')
+    print('\nОШИБКА: Вводите х и y координаты, в формате ’x;y’')
     exit()
-
 
 try:
-    x2,y2=input('Введите координаты ВТОРОЙ точки (x;y): ').split(';')
-    x2,y2=map(float,(x2,y2))
-    if x1-x2==0:
-        raise ZeroDivisionError
+    x2, y2 = map(float,
+                 input('\nВведите координаты ВТОРОЙ точки (x;y): ').split(';'))
+
 except ValueError:
-    print('ОШИБКА: Введите х и y координаты, в формате ’x;y’')
-    exit()
-except ZeroDivisionError:
-    print('ОШИБКА деления на 0: значения x1 и x2 совпадают')
+    print('\nОШИБКА: Вводите х и y координаты, в формате ’x;y’')
     exit()
 
+k, b = equation(x1, y1, x2, y2)
 
-k,b=equation(x1,y1,x2,y2)
-
-if b>0:
-    print(f'Уравнение: y = {k} * x +{b}')
+if b > 0:
+    print(f'\nУравнение: y = {k} * x +{b}')
 else:
-    print(f'Уравнение: y = {k} * x {b}')
-
-
-
-
+    print(f'\nУравнение: y = {k} * x {b}')
--- HW1/Task7.py
+++ HW1/Task7.py
@@ -1,2 +1,41 @@
 # 7. По длинам трех отрезков, введенных пользователем, определить возможность существования треугольника, составленного из этих отрезков. Если такой треугольник существует, то определить, является ли он разносторонним, равнобедренным или равносторонним.
 
+def triangle_type(a, b, c):
+  try:
+      if a + b < c or a + c < b or b + c < a:
+        raise AttributeError
+      elif a == b or b == c or a == c:
+          if a == b == c:
+              return '\nТреугольник - раВносторонний'
+          return '\nТреугольник - равнобедренный'
+      else:
+          return '\nТреугольник - раЗносторонний'
+  except AttributeError:
+      print(
+          '\nОШИБКА: Сумма любых двух отрезков должна быть больше длины 3-го отрезка'
+      )
+      exit()
+  
+
+def test():
+    assert triangle_type(3,4,5)=='\nТреугольник - раЗносторонний', 'icorrect'
+    assert triangle_type(3,3,5)=='\nТреугольник - равнобедренный', 'icorrect'
+    assert triangle_type(5,5,5)=='\nТреугольник - раВносторонний', 'icorrect'
+    #assert triangle_type(1,1,5)==False, 'icorrect'
+    print(f'Test: OK\n')
+
+
+test()
+try:
+    a, b, c = map(
+        float,
+        input('Введите длины 3-х отрезков для проверки треугольника: ').split(
+            ' '))
+except ValueError:
+    print(
+        '\nОШИБКА: Введите цифровые значение длин для каждой стороны треугольника, разделив их пробелом'
+    )
+    exit()
+
+tr_type = triangle_type(a, b, c)
+print(tr_type)
